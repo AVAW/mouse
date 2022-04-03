e463fdb65ce005f3966accd618942b930ef76ab3
Arek Wójcik <wojcik.arkadiusz.a@gmail.com>
2022-04-03 15:46:36 +0200

    jQuery and navbar

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,14 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 import 'bulma';
+
+const $ = require('jquery');
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+});
