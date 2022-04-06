175128735f8357658bde7ee212799ddb26fc45f0
Arek Wójcik <wojcik.arkadiusz.a@gmail.com>
2022-04-06 20:40:49 +0200

    i18n

---
+++ src/Twig/Locales.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+
+class Locales extends AbstractExtension implements GlobalsInterface
+{
+    private array $locales;
+
+    public function __construct(array $locales)
+    {
+        $this->locales = $locales;
+    }
+
+    public function getGlobals(): array
+    {
+        return [
+            'locales' => $this->locales,
+        ];
+    }
+
+    public function getName(): string
+    {
+        return 'Locales_extension';
+    }
+
+}
